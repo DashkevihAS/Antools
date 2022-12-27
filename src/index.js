1e217b76f69992a02829fc07edfad528a6e53f23
DashkevichAS <antonsnov0604@gmail.com>
2022-12-27 18:27:03 +0300

    init swiper-slider

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
+import Swiper, { Navigation, Pagination } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/pagination';
+
 import './index.html';
 import './index.scss';
+
+const swiper = new Swiper('.slider__container', {
+  modules: [Navigation, Pagination],
+  loop: true,
+  navigation: {
+    nextEl: '.slider__arrow-next',
+    prevEl: '.slider__arrow-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
