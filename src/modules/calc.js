d641c4631462878b58b15167520cf44dd3296187
DashkevichAS <antonsnov0604@gmail.com>
2022-12-27 13:36:47 +0300

    done with static slider

--- src/modules/calc.js
+++
@@ -1,8 +0,0 @@
-export const mult = (a, b) => a * b;
-export const sum = async (a, b) => a + b;
-
-document
-  .querySelector('.navigation__item_popup')
-  .addEventListener('click', () => {
-    console.log('click');
-  });
